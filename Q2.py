5e3bf7b192337ac7dcf522d115474386019cfbf6
Ahn <Ahn@gmail.com>
2023-02-02 16:04:08 +0900

    Q2

---
+++ Q2.py
@@ -0,0 +1,14 @@
+# Q2 Answer template
+
+N = int(input())
+count=0
+value=N
+while True:    
+        N=(N%10)*10+(((N%10)+(N//10))%10)
+
+        count+=1
+        if value==N:
+            break
+        
+print(count)
+   
